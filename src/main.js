35585efec690e9a4d56ec23cbb04ca48b6339f33
Silvia <silvia.jisu.oh@gmail.com>
2021-06-06 20:35:54 +0900

    feat: localStorage 안에 currentUser있을 경우만 시계,todo출력

--- src/main.js
+++ src/main.js
@@ -1,4 +1,42 @@
 "use strict";
+
+const input = document.querySelector(".questionInput");
+const USER_LS = "currentUser";
+const SHOW = "showing";
+const clockAndTodo = document.querySelector(".clockAndTodo");
+const nameSpan = document.querySelector(".name");
+const question = document.querySelector(".question");
+
+function printContent(currentUser) {
+  question.classList.remove("showing");
+  clockAndTodo.classList.add("showing");
+  nameSpan.innerText = `Hello ${currentUser}`;
+}
+
+function saveCurrentUser() {
+  input.addEventListener("keydown", (e) => {
+    if (e.keyCode === 13) {
+      const value = input.value;
+      localStorage.setItem(USER_LS, value);
+      printContent(value);
+    }
+  });
+}
+
+function showQuestion() {
+  question.classList.add(SHOW);
+}
+
+function loadclockAndTodo() {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+    showQuestion();
+    saveCurrentUser();
+  } else {
+    printContent(currentUser);
+  }
+}
+
 function setClock() {
   const today = new Date();
   const hour = today.getHours();
@@ -14,3 +52,5 @@ function showClock(time) {
   spanClock.innerHTML = time;
 }
 setClock();
+
+loadclockAndTodo();
