3316f3ec01ee5627417406a61f536b74c4239f3a
silviaoh <silvia.jisu.oh@gmail.com>
2022-04-06 20:34:25 +0900

    add: [LogIn] submit event

--- javascript-momonto/src/main.js
+++ javascript-momonto/src/main.js
@@ -1,56 +1,14 @@
-"use strict";
+/* login form */
+// input과 버튼 찾기
+const loginForm = document.querySelector('#login_form');
+const loginInput = document.querySelector('#login_form input');
 
-const input = document.querySelector(".questionInput");
-const USER_LS = "currentUser";
-const SHOW = "showing";
-const clockAndTodo = document.querySelector(".clockAndTodo");
-const nameSpan = document.querySelector(".name");
-const question = document.querySelector(".question");
-
-function printContent(currentUser) {
-  question.classList.remove("showing");
-  clockAndTodo.classList.add("showing");
-  nameSpan.innerText = `Hello ${currentUser}`;
+function onLoginSubmit(event) {
+	// 어떤 function에 대해 브라우저가 기본적으로 수행하는 동작을 막는다.
+	// 자식 <li></li> 태그에 클릭 이벤트를 실행하고 만약 부모에도 클릭 이벤트가 있다면 타고타고 올라가 부모의 이벤트까지 의도하지 않게 실행되어 버림(버블링)
+	// e.stopPropagation -> 부모태그로의 이벤트 전파를 중지하라는 의미.
+	event.preventDefault();
+	console.log(loginInput.value);
 }
 
-function saveCurrentUser() {
-  input.addEventListener("keydown", (e) => {
-    if (e.keyCode === 13) {
-      const value = input.value;
-      localStorage.setItem(USER_LS, value);
-      printContent(value);
-    }
-  });
-}
-
-function showQuestion() {
-  question.classList.add(SHOW);
-}
-
-function loadclockAndTodo() {
-  const currentUser = localStorage.getItem(USER_LS);
-  if (currentUser === null) {
-    showQuestion();
-    saveCurrentUser();
-  } else {
-    printContent(currentUser);
-  }
-}
-
-function setClock() {
-  const today = new Date();
-  const hour = today.getHours();
-  const minutes = today.getMinutes();
-  const todayTime = `${hour}:${minutes}`;
-
-  showClock(todayTime);
-  setTimeout(setClock, 1000);
-}
-
-function showClock(time) {
-  const spanClock = document.querySelector(".time");
-  spanClock.innerHTML = time;
-}
-setClock();
-
-loadclockAndTodo();
+loginForm.addEventListener('submit', onLoginSubmit);
